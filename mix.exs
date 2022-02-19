545d6c2bf8275a3036885199065002695809e331
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2022-02-18 21:05:11 -0300

    :art: after mix format

--- mix.exs
+++ mix.exs
@@ -53,7 +53,7 @@ defmodule Ifood.MixProject do
       {:plug_cowboy, "~> 2.5"},
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
       {:excoveralls, "~> 0.14.4", only: :test},
-      {:sobelow, "~> 0.11.1", only: :dev},
+      {:sobelow, "~> 0.11.1", only: :dev}
     ]
   end
 
