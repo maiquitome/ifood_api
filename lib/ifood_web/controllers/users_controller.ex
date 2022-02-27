2bd8a8c5c0374618f52b64169c401e6070ce2f92
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2022-02-26 21:31:10 -0300

    :sparkles: add user show action

--- lib/ifood_web/controllers/users_controller.ex
+++ lib/ifood_web/controllers/users_controller.ex
@@ -16,4 +16,12 @@ defmodule IfoodWeb.UsersController do
       |> render("create.json", user: user)
     end
   end
+
+  def show(conn, %{"id" => id}) do
+    with {:ok, %User{} = user} <- Accounts.get_user_by_id(id) do
+      conn
+      |> put_status(:ok)
+      |> render("user.json", user: user)
+    end
+  end
 end
--- test/ifood_web/views/users_view_test.exs
+++ test/ifood_web/views/users_view_test.exs
@@ -30,4 +30,27 @@ defmodule IfoodWeb.UsersViewTest do
              }
            } = response
   end
+
+  test "renders user.json" do
+    user = build(:user)
+
+    response = render(UsersView, "user.json", user: user)
+
+    assert %{
+             user: %User{
+               birthdate: "13/05/1993",
+               cpf: "12345678911",
+               email: "maiqui@email.com",
+               first_name: "Maiqui",
+               id: "b721fcad-e6e8-4e8f-910b-6911f2158b4a",
+               inserted_at: _inserted_at,
+               last_name: "Tomé",
+               password: "123456",
+               password_confirmation: "123456",
+               password_hash: _password_hash,
+               phone_number: "5496159531",
+               updated_at: _updated_at
+             }
+           } = response
+  end
 end
